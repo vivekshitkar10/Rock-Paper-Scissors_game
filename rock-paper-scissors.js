ca0aef5caee1fcf8a7c2103f31bf637219bbcbd7
Vivek Shitkar <92878509+vivekshitkar10@users.noreply.github.com>
2023-07-07 02:33:17 +0530

    Update rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -79,8 +79,8 @@ let score =
     document.querySelector('.js-result').innerHTML = result;
   
     document.querySelector('.js-moves').innerHTML = `You
-  <img src="images/${playerMove}-emoji.png" class="move-icon">
-  <img src="images/${computerMove}-emoji.png" class="move-icon">
+  <img src="${playerMove}-emoji.png" class="move-icon">
+  <img src="${computerMove}-emoji.png" class="move-icon">
   Computer`;
   }
   
